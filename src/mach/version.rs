db2f39e29ce45f22edd6bae733c14536c25815c9
Camden Narzt <c.narzt@me.com>
2026-04-06 22:50:10 -0600

    try to be nostd friendly

--- src/mach/version.rs
+++ src/mach/version.rs
@@ -6,10 +6,12 @@ use crate::error::Error;
 use crate::mach::cputype::{CPU_TYPE_ARM64, CPU_TYPE_X86_64};
 use crate::mach::load_command::CommandVariant;
 use crate::mach::{Mach, MachO, SingleArch};
-use std::cmp::Ordering;
-use std::collections::VecDeque;
-use std::str::FromStr;
-use std::{env, fmt};
+if_std! {
+    use std::cmp::Ordering;
+    use std::collections::VecDeque;
+    use std::str::FromStr;
+    use std::{env, fmt};
+}
 
 #[derive(Eq, Debug)]
 pub struct Version {
@@ -18,35 +20,37 @@ pub struct Version {
     pub patch: u32,
 }
 
-impl fmt::Display for Version {
-    // This trait requires `fmt` with this exact signature.
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
-        // operation succeeded or failed. Note that `write!` uses syntax which
-        // is very similar to `println!`.
-        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
-    }
-}
-
-impl Ord for Version {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let mao = self.major.cmp(&other.major);
-        let mio = self.minor.cmp(&other.minor);
-        let pao = self.patch.cmp(&other.patch);
-        if mao == Ordering::Equal && mio == Ordering::Equal {
-            pao
-        } else if mao == Ordering::Equal {
-            mio
-        } else {
-            mao
+if_std! {
+    impl fmt::Display for Version {
+        // This trait requires `fmt` with this exact signature.
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            // Write strictly the first element into the supplied output
+            // stream: `f`. Returns `fmt::Result` which indicates whether the
+            // operation succeeded or failed. Note that `write!` uses syntax which
+            // is very similar to `println!`.
+            write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
         }
     }
-}
 
-impl PartialOrd for Version {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+    impl Ord for Version {
+        fn cmp(&self, other: &Self) -> Ordering {
+            let mao = self.major.cmp(&other.major);
+            let mio = self.minor.cmp(&other.minor);
+            let pao = self.patch.cmp(&other.patch);
+            if mao == Ordering::Equal && mio == Ordering::Equal {
+                pao
+            } else if mao == Ordering::Equal {
+                mio
+            } else {
+                mao
+            }
+        }
+    }
+
+    impl PartialOrd for Version {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
     }
 }
 
@@ -55,23 +59,24 @@ impl PartialEq for Version {
         self.major == other.major && self.minor == other.minor && self.patch == other.patch
     }
 }
+if_std! {
+    impl FromStr for Version {
+        type Err = Error;
 
-impl FromStr for Version {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut parts = s
             .trim()
             .split('.')
             .map(|p| p.parse::<u32>().unwrap())
             .take(3)
             .collect::<VecDeque<u32>>();
 
-        Ok(Self {
-            major: parts.pop_front().unwrap(),
-            minor: parts.pop_front().unwrap_or(0),
-            patch: parts.pop_front().unwrap_or(0),
-        })
+            Ok(Self {
+                major: parts.pop_front().unwrap(),
+                minor: parts.pop_front().unwrap_or(0),
+                patch: parts.pop_front().unwrap_or(0),
+            })
+        }
     }
 }
 
@@ -105,26 +110,28 @@ impl From<MachO<'_>> for Version {
     }
 }
 
-impl From<Mach<'_>> for Version {
-    fn from(b: Mach) -> Self {
-        match b {
-            Mach::Binary(b) => Version::from(b),
-            Mach::Fat(f) => {
-                match f
+if_std! {
+    impl From<Mach<'_>> for Version {
+        fn from(b: Mach) -> Self {
+            match b {
+                Mach::Binary(b) => Version::from(b),
+                Mach::Fat(f) => {
+                    match f
                     .find(|r| {
                         r.unwrap().cputype
-                            == match env::var("CARGO_CFG_TARGET_ARCH").as_deref() {
-                                Ok("x86_64") => CPU_TYPE_X86_64,
-                                Ok("aarch64") => CPU_TYPE_ARM64,
-                                _ => panic!("unknown arch"),
-                            }
+                        == match env::var("CARGO_CFG_TARGET_ARCH").as_deref() {
+                            Ok("x86_64") => CPU_TYPE_X86_64,
+                            Ok("aarch64") => CPU_TYPE_ARM64,
+                            _ => panic!("unknown arch"),
+                        }
                     })
                     .unwrap()
                     .ok()
                     .unwrap()
-                {
-                    SingleArch::MachO(b) => Version::from(b),
-                    SingleArch::Archive(_) => panic!("lib is an archive?"),
+                    {
+                        SingleArch::MachO(b) => Version::from(b),
+                        SingleArch::Archive(_) => panic!("lib is an archive?"),
+                    }
                 }
             }
         }
